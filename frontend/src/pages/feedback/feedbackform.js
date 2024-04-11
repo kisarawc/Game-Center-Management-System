957e70264ea37fe73f5d40f50f0e96189522b93e
Radeesa <155997978+Radeesa@users.noreply.github.com>
2024-04-11 06:34:32 +0530

    Update feedbackform.js

--- frontend/src/pages/feedback/feedbackform.js
+++ frontend/src/pages/feedback/feedbackform.js
@@ -5,40 +5,40 @@ const feedbackform = props => {
     return (
         <Grid
 
-            sx={{
-                backgroundColor: '#ffffff',
-                marginBottom: '30px',
-                display: 'block',
-
-
-            }}
 
 
         >
             <Grid item xs={12}>
-                <Typography component={'h1'} sx={{ color: '#000000' }} >feedbackform</Typography>
+                <Typography component={'center'} sx={{ fontSize: '25px' }} >Feedback Form</Typography>
                 <Grid />
-                <Grid item xs={12} sm={6} sx={{ display: 'flex' }}>
+                <Grid item xs={12} sm={6} sx={{ display: 'flex' }} >
                     <Typography
                         component={'label'}
                         htmlFor="id"
+                        align='center'
+
                         sx={{
                             color: '#000000',
-                            marginRight: '20px',
-                            fontSize: '16px',
-                            width: '100px',
+                            marginRight: '15px',
+                            fontSize: '20px',
+                            width: '200px',
                             display: 'block',
+                            marginBottom: '15px',
+                            padding: '20px',
+
 
                         }}
                     >
-                        ID
+                        Game Name:
 
                     </Typography>
 
                     <Input
-                        type="number"
-                        id='id'
-                        name="id"
+                        type="text"
+                        id='Gamename'
+                        name="Gamename"
+
+
                         sx={{ width: '400px' }}
                         value={''}
                         onChange={e => { }}
@@ -47,24 +47,92 @@ const feedbackform = props => {
 
                 </Grid>
             </Grid>
+            <Grid item xs={12} sm={6} sx={{ display: 'flex' }}>
+                <Typography
+                    component={'label'}
+                    htmlFor="id"
+                    align='center'
+                    sx={{
+                        color: '#000000',
+                        marginRight: '15px',
+                        fontSize: '20px',
+                        width: '200px',
+                        display: 'block',
+                        marginBottom: '15px',
+                        padding: '20px',
+
+                    }}
+                >
+                    Your review:
+
+                </Typography>
+
+                <Input
+                    type="text Area"
+                    id='review'
+                    name="review"
+                    sx={{ width: '400px' }}
+                    value={''}
+                    onChange={e => { }}
+                />
+
+
+            </Grid>
+
+
 
             <Grid item xs={12} sm={6} sx={{ display: 'flex' }}>
 
                 <Typography
-                    component={'label'}
+
                     htmlFor="id"
+                    align='center'
                     sx={{
                         color: '#000000',
-                        marginRight: '20px',
-                        fontSize: '16px',
-                        width: '100px',
+                        marginRight: '15px',
+                        fontSize: '20px',
+                        width: '200px',
                         display: 'block',
+                        marginBottom: '15px',
+                        padding: '20px',
 
                     }}
                 >
-                    Name
+                    upload Pictures:
 
                 </Typography>
+                <Input
+                    type="file"
+                    id='picture'
+                    name="picture"
+                    sx={{ width: '400px' }}
+                    value={''}
+                    onChange={e => { }}
+                />
+
+
+            </Grid>
+            <Grid item xs={12} sm={6} sx={{ display: 'flex' }}>
+                <Typography
+                    component={'label'}
+                    htmlFor="id"
+                    align='center'
+                    sx={{
+                        color: '#000000',
+                        marginRight: '15px',
+                        fontSize: '20px',
+                        width: '200px',
+                        display: 'block',
+                        marginBottom: '15px',
+                        padding: '20px',
+
+
+                    }}
+                >
+                    Name:
+
+                </Typography>
+
                 <Input
                     type="text"
                     id='name'
@@ -76,21 +144,53 @@ const feedbackform = props => {
 
 
             </Grid>
+            <Grid item xs={12} sm={6} sx={{ display: 'flex' }}>
+                <Typography
+                    component={'label'}
+                    htmlFor="id"
+                    align='center'
+                    sx={{
+                        color: '#000000',
+                        marginRight: '15px',
+                        fontSize: '20px',
+                        width: '200px',
+                        display: 'block',
+                        marginBottom: '15px',
+                        padding: '20px',
+
+                    }}
+                >
+                    Email:
+
+                </Typography>
+
+                <Input
+                    type="email"
+                    id='email'
+                    name="email"
+                    sx={{ width: '400px' }}
+                    value={''}
+                    onChange={e => { }}
+                />
+
+
+            </Grid>
             <button
                 sx={{
-                    margin: 'auto',
-                    marginBottom: '20px',
+                    marginRight: '200px',
+                    marginBottom: '200px',
                     backgroundColor: '#00c6e6',
                     color: '#000000',
-                    marginleft: '15px',
-                    marginTop: '20px',
+                    marginleft: '200px',
+                    marginTop: '300px',
+                    width: '200px',
                     '&:hover': {
                         Opacity: '0.7',
                         backgroundColor: '#00c6e6',
                     }
                 }}
             >
-                Add
+                Submit
 
             </button>
 
