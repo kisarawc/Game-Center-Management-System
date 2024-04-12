052b21f7c5b6cbb0396512e9aa22d881946a72fb
Radeesa <155997978+Radeesa@users.noreply.github.com>
2024-04-12 21:59:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,15 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const eventRoutes = require('./routes/Ravindu/eventRoutes');
 const commentRoutes = require('./routes/Ravindu/commentRoutes');
+const feedbackRoutes = require('./routes/Radeesa/feedbackRoutes');
 require('dotenv').config();
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json()); // Middleware to parse JSON request bodies
-
 
 // Connect to MongoDB Atlas
 mongoose.connect(process.env.MONGODB_URL, {
@@ -26,7 +26,7 @@ mongoose.connect(process.env.MONGODB_URL, {
         // Use event routes
         app.use('/api/events', eventRoutes);
         app.use('/api/events', commentRoutes);// Using '/api' as the base URL for event routes
-
+        app.use('/api/feedbacks', feedbackRoutes);
 
         // Start the server
         app.listen(PORT, () => {
