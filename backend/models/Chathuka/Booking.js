9852790dfb15296245185b833703c46c93d9d359
IT22582874 <127914126+kisarawc@users.noreply.github.com>
2024-04-23 09:32:36 +0530

    new

--- backend/models/Chathuka/Booking.js
+++ backend/models/Chathuka/Booking.js
@@ -4,7 +4,7 @@ const bookingSchema = new mongoose.Schema({
   date: { type: Date },
   start_time: { type: Date },
   end_time: { type: Date },
-  duration: { type: Number }, // Representing duration in minutes
+  duration: { type: Number },
   message_request: { type: String },
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   game_name: { type: String }, 
